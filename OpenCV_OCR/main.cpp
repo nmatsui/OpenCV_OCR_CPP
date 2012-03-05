53f3374747858b026e3adbd75c74cfa5edc31c93
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2012-03-05 20:10:08 +0900

    Initial Commit

---
+++ OpenCV_OCR/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCV_OCR
+//
+//  Created by cube on 12/03/05.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
